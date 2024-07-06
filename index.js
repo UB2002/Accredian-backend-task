b010f278ca20cf5f01b597343093fa46962b6e7e
UB2002 <udaybhaskar0532@gmail.com>
2024-07-06 21:44:28 +0530

    screenshots

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const cors = require('cors');
-const nodemailer = require('nodemailer');
+
 
 const prisma = new PrismaClient();
 const app = express();
@@ -9,32 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Configure Nodemailer
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS
-  }
-});
-
-// Send an email using Nodemailer
-const sendEmail = (form) => {
-  const mailOptions = {
-    from: 'noreply@yourdomain.com',
-    to: form.email,
-    subject: 'Thank you for the referral!',
-    text: `Thank you ${form.name} for referring ${form.course} with code ${form.referralCode}.`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending email:', error);
-    } else {
-      console.log('Email sent:', info.response);
-    }
-  });
-};
 
 // Root route
 app.get('/', (req, res) => {
@@ -51,13 +25,7 @@ app.post('/api/referrals', async (req, res) => {
       data: { name, email, referralCode, course }
     });
 
-    // Send an email
-    sendEmail({
-      name,
-      email,
-      referralCode,
-      course
-    });
+    ;
 
     res.status(201).json(newReferral);
   } catch (error) {
